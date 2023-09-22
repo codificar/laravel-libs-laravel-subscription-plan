d087f3c1f94a344622c52a0280c83c2992ebe145
Randler Ferraz Almeida <randler.ferraz@codificar.com.br>
2023-09-22 16:43:11 -0300

    correção tradução es-py

---
+++ src/resources/lang/es-py/signature.php
@@ -0,0 +1,29 @@
+<?php
+return array(
+    'debit_email_send'                  =>  'Enlace a pago de suscripción',
+    'email_payment'                     =>  'Pago de la Suscripción',
+    'signature_cancelation'             =>  'Suscripción cancelada',
+    'email_cancelation'                 =>  'Su suscripción ha sido cancelada.',
+    'signature'                         =>  'Suscripción',
+    'plural'                            =>  'Suscripciónes',
+
+    'expiration_date'                   =>  'Fecha de vencimiento',
+    'id'                                =>  'Id',
+    'name'                              =>  'Nombre',
+    'client_name'                       =>  'Nombre del cliente',
+    'signature_price'                   =>  'Precio de la firma',
+    'filter'                            =>  'Filtro',
+    'action'                            =>  'Acción',
+    'debit_collection'                  =>  'Hacer cobrar',
+    'change_activity'                   =>  'Cambiar situación',
+    'provider'                          =>  'Conductor',
+    'initial_date'                      =>  'Fecha de la firma',
+    'situation'                         =>  'Situación',
+    'yes'                               =>  'Sí',
+    'no'                                =>  'No',
+    'active'                            =>  'Activo',
+    'inactive'                          =>  'Inactivo',
+    'cards'                             =>  'tarjetas',
+    'filters'                           =>  'Filtros',
+   
+);
--- tests/translate/LibPlanLanguageSpanishTest.php
+++ tests/translate/LibPlanLanguageSpanishTest.php
@@ -19,7 +19,7 @@ class LibPlanLanguageSpanishTest extends TestCase
         
         $path = base_path() . '/vendor/codificar/subscription-plan/src/resources/lang';
         $directoryPt = "$path/pt-br/*.php";
-        $directoryEs = "$path/es/*.php";
+        $directoryEs = "$path/es-py/*.php";
         $filesPt = glob($directoryPt);
         $filesEs = glob($directoryEs);
 
