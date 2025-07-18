fe9a301bd16e69acc8323fd2517dfe85bc773b08
oto.luz <oto.luz@codificar.com.br>
2025-07-18 13:12:38 -0300

    fix ao translate bug

---
+++ src/resources/lang/ao/signature.php
@@ -0,0 +1,29 @@
+<?php
+return array(
+    'debit_email_send'                  =>  'Link para pagamento de assinatura',
+    'email_payment'                     =>  'Pagamento de Assinatura',
+    'signature_cancelation'             =>  'Assinatura cancelada',
+    'email_cancelation'                 =>  'A sua assinatura foi cancelada.',
+    'signature'                         =>  'Assinatura',
+    'plural'                            =>  'Assinaturas',
+    
+    'expiration_date'                   =>  'Vencimento',
+    'id'                                =>  'Id',
+    'name'                              =>  'Nome',
+    'client_name'                       =>  'Nome do Cliente',
+    'signature_price'                   =>  'Preço da assinatura',
+    'filter'                            =>  'Filtro',
+    'action'                            =>  'Acções',
+    'debit_collection'                  =>  'Fazer Cobrança',
+    'change_activity'                   =>  'Muda situação',
+    'provider'                          =>  'Condutor',
+    'initial_date'                      =>  'Data de assinatura',
+    'situation'                         =>  'Situação',
+    'yes'                               =>  'Sim',
+    'no'                                =>  'Não',
+    'active'                            =>  'Activo',
+    'inactive'                          =>  'Inactivo',
+    'cards'                             =>  'Cartões',
+    'filters'                           =>  'Filtros',
+    
+);
